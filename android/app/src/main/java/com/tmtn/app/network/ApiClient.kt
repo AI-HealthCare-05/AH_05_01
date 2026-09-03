7c955d270948c6945913105932f0fdc4021197e9
jrohan93 <sapienc11@gmail.com>
2026-09-03 18:23:12 +0900

    fix: merge 중 유실된 tuntunScoreApi 복구

--- android/app/src/main/java/com/tmtn/app/network/ApiClient.kt
+++ android/app/src/main/java/com/tmtn/app/network/ApiClient.kt
@@ -6,6 +6,7 @@ import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
+import com.tmtn.app.network.TuntunScoreApi
 
 object ApiClient {
 
@@ -66,7 +67,7 @@ object ApiClient {
     val cardHomeApi: CardHomeApi by lazy { retrofit.create(CardHomeApi::class.java) }
     val recordApi: RecordApi by lazy { retrofit.create(RecordApi::class.java) }
     val profileApi: ProfileApi by lazy { retrofit.create(ProfileApi::class.java) }
-
+    val tuntunScoreApi: TuntunScoreApi by lazy { retrofit.create(TuntunScoreApi::class.java) }
     /**
      * 실제 온보딩 화면(A03~A10)이 생겨서, 이제 이 함수는 "온보딩 건너뛰고 바로 기능 테스트"
      * 하고 싶을 때 쓰는 용도로만 남겨둠. 최초 온보딩 흐름 자체는 OnboardingState가 담당.
