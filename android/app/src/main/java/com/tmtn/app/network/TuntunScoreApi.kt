1355091100f4981027ea7c10e6e0e7d326e84c3f
김지현 <rlarha3288@gmail.com>
2026-09-07 15:37:22 +0900

    feat: connect tuntun score v2 Android screens

--- android/app/src/main/java/com/tmtn/app/network/TuntunScoreApi.kt
+++ android/app/src/main/java/com/tmtn/app/network/TuntunScoreApi.kt
@@ -3,6 +3,7 @@ package com.tmtn.app.network
 import com.tmtn.app.network.model.ScoreInputsResponse
 import com.tmtn.app.network.model.TmtnScoreResponse
 import com.tmtn.app.network.model.TuntunScoreOrEligibilityResponse
+import com.tmtn.app.network.model.TuntunScoreV2Response
 import retrofit2.Response
 import retrofit2.http.GET
 
@@ -10,6 +11,9 @@ interface TuntunScoreApi {
 
     @GET("tuntun-score")
     suspend fun getTuntunScore(): Response<TuntunScoreOrEligibilityResponse>
+
+    @GET("tuntun-score/v2")
+    suspend fun getTuntunScoreV2(): Response<TuntunScoreV2Response>
 
     @GET("tuntun-score/about")
     suspend fun getTuntunScoreAbout(): Response<TmtnScoreResponse>
